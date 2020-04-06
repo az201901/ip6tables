6d2391c5428f33a774cecd043192dbc91a1d38b9
az201901 <60432561+az201901@users.noreply.github.com>
2020-04-06 23:40:10 +0900

    Update ip6_wifi_h.sh

--- ip6_wifi_h.sh
+++ ip6_wifi_h.sh
@@ -12,3 +12,12 @@ port_http='80'
 port_https='443'
 port_ntp='123'
 
+port_LLMNR='5355'
+
+# ---------------------------
+icmpv6_V2_MLR='143'
+
+# ---------------------------
+DHCP_Relay='ff02::1:2'
+DHCPv6_servers='ff02::1:3'
+
